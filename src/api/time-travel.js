8837670ab2c738dc74c3a363c1b9e704d991dd8d
Benedicte Raae <raae@raae.codes>
2021-10-26 11:11:47 +0200

    Solution to test showing "oslo" and "    Oslo" etc would not work.

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -1,11 +1,11 @@
 const CORRECT_YEAR = "2026";
-const CORRECT_CITY = "Oslo";
+const CORRECT_CITY = "OSLO";
 
 export default function handler(req, res) {
   const { city, year } = req.body;
   let message = `You time-travelled to ${city}, in the year ${year},  `;
 
-  if (city === CORRECT_CITY && year === CORRECT_YEAR) {
+  if (city.trim().toUpperCase() === CORRECT_CITY && year === CORRECT_YEAR) {
     message += `where you found the princess!`;
   } else {
     message += `where you did NOT find the princess!`;
