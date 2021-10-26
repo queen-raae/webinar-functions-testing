ab019a5869eea16b21232714d43d22b48d900d39
Benedicte Raae <raae@raae.codes>
2021-10-26 11:16:10 +0200

    Solution to properly handle undefined body params

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -6,13 +6,17 @@ export default function handler(req, res) {
     const { city, year } = req.body;
     let message = `You time-travelled to ${city}, in the year ${year},  `;
 
-    if (city.trim().toUpperCase() === CORRECT_CITY && year === CORRECT_YEAR) {
-      message += `where you found the princess!`;
-    } else {
-      message += `where you did NOT find the princess!`;
-    }
+    if (city && year) {
+      if (city.trim().toUpperCase() === CORRECT_CITY && year === CORRECT_YEAR) {
+        message += `where you found the princess!`;
+      } else {
+        message += `where you did NOT find the princess!`;
+      }
 
-    res.json({ message });
+      res.json({ message });
+    } else {
+      res.status(422).json({ message: "Invalid body params" });
+    }
   } catch (error) {
     console.warn(error.message);
     res.status(500).json({ message: "Faulty TimeShip!" });
