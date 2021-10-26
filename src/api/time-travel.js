05668dd54c58571f5c78e471269e0669b7fc8747
Benedicte Raae <raae@raae.codes>
2021-10-26 11:13:35 +0200

    Solution to catch unexpected errors

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -2,14 +2,19 @@ const CORRECT_YEAR = "2026";
 const CORRECT_CITY = "OSLO";
 
 export default function handler(req, res) {
-  const { city, year } = req.body;
-  let message = `You time-travelled to ${city}, in the year ${year},  `;
+  try {
+    const { city, year } = req.body;
+    let message = `You time-travelled to ${city}, in the year ${year},  `;
 
-  if (city.trim().toUpperCase() === CORRECT_CITY && year === CORRECT_YEAR) {
-    message += `where you found the princess!`;
-  } else {
-    message += `where you did NOT find the princess!`;
+    if (city.trim().toUpperCase() === CORRECT_CITY && year === CORRECT_YEAR) {
+      message += `where you found the princess!`;
+    } else {
+      message += `where you did NOT find the princess!`;
+    }
+
+    res.json({ message });
+  } catch (error) {
+    console.warn(error.message);
+    res.status(500).json({ message: "Faulty TimeShip!" });
   }
-
-  res.json({ message });
 }
