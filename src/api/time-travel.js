0fd1955100d678004382269b2a4ce7a3131ec300
Benedicte Raae <raae@raae.codes>
2021-10-26 11:25:40 +0200

    End up with something like this.

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -1,10 +1,22 @@
-const CORRECT_YEAR = "2026";
+const CORRECT_YEAR = 2026;
 const CORRECT_CITY = "OSLO";
 
 export default function handler(req, res) {
   try {
     const { city, year } = req.body;
     let message = `You time-travelled to ${city}, in the year ${year},  `;
+
+    if (!city || !year) {
+      throw createError(422, "Missing body params");
+    }
+
+    if (parseInt(year) === "NaN") {
+      throw createError(422, "Year not a number");
+    }
+
+    if (typeof city !== "string") {
+      throw createError(422, "City not a string");
+    }
 
     if (city && year) {
       if (city.trim().toUpperCase() === CORRECT_CITY && year === CORRECT_YEAR) {
@@ -18,7 +30,15 @@ export default function handler(req, res) {
       res.status(422).json({ message: "Invalid body params" });
     }
   } catch (error) {
-    console.warn(error.message);
-    res.status(500).json({ message: "Faulty TimeShip!" });
+    let { message, expose, status = 500 } = error;
+    console.warn(status, message);
+    res.status(status).json({ message: expose ? message : "Faulty TimeShip!" });
   }
 }
+
+const createError = (status, message) => {
+  const error = new Error(message);
+  error.status = status;
+  error.expose = true;
+  return error;
+};
