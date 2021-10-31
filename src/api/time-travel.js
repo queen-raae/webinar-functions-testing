3d0b4f97a774b85cc544f5f648f8ab41b245a14e
programmiri <me@programmiri.rocks>
2021-10-31 18:05:19 +0100

    6. test upper/lower cases does not matter

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -1,12 +1,15 @@
 const CORRECT_YEAR = "2026";
-const CORRECT_CITY = "Oslo";
+const CORRECT_CITY = "OSLO";
 
 export default function handler(req, res) {
   const { year, city } = req.body;
 
   let message = `You time-traveled to ${city}, in the year ${year}, `;
 
-  if (city === CORRECT_CITY && year === CORRECT_YEAR) {
+  if (
+    city.trim().toUpperCase() === CORRECT_CITY &&
+    year.trim() === CORRECT_YEAR
+  ) {
     message += `where you found the princess!`;
   } else {
     message += `where you did NOT find the princess!`;
