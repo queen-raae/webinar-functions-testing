a12b2caefaedd5f6af60a4d655e8661b444ca303
programmiri <me@programmiri.rocks>
2021-10-31 17:56:00 +0100

    5. test - find princess in oslo only with right year

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -1,3 +1,4 @@
+const CORRECT_YEAR = "2026";
 const CORRECT_CITY = "Oslo";
 
 export default function handler(req, res) {
@@ -5,7 +6,7 @@ export default function handler(req, res) {
 
   let message = `You time-traveled to ${city}, in the year ${year}, `;
 
-  if (city === CORRECT_CITY) {
+  if (city === CORRECT_CITY && year === CORRECT_YEAR) {
     message += `where you found the princess!`;
   } else {
     message += `where you did NOT find the princess!`;
