4f7c9a1f23f16ba02e2d2ca375c0956964f74745
programmiri <me@programmiri.rocks>
2021-10-31 17:49:13 +0100

    3. test - do not find the princess

--- src/api/time-travel.js
+++ src/api/time-travel.js
@@ -1,6 +1,14 @@
-export default function handler(req, res) {
-  const { city } = req.body;
-  const { year } = req.body;
+const CORRECT_CITY = "Oslo";
 
-  res.send(`You time-traveled to ${city}, in the year ${year}`);
+export default function handler(req, res) {
+  const { year, city } = req.body;
+
+  let message = `You time-traveled to ${city}, in the year ${year}, `;
+
+  if (city === CORRECT_CITY) {
+    message += `where you found the princess!`;
+  } else {
+    message += `where you did NOT find the princess!`;
+  }
+  res.send(message);
 }
