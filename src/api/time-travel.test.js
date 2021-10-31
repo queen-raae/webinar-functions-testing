7407312dd0edd2be79a231eaac1fba2513aa1dbe
programmiri <me@programmiri.rocks>
2021-10-31 17:53:27 +0100

    4. test - find princess in oslo

--- src/api/time-travel.test.js
+++ src/api/time-travel.test.js
@@ -7,25 +7,49 @@ describe("time-travel.js", () => {
     jest.clearAllMocks();
   });
 
-  it("does not find the princess in Barcelona 1997", async () => {
-    const testYear = "1997";
-    const testCity = "Barcelona";
+  describe("checks if it finds the princess in specific year and city", () => {
+    it("does find the princess in Oslo 2026", async () => {
+      const testYear = "2026";
+      const testCity = "Oslo";
 
-    const req = {
-      body: {
-        year: testYear,
-        city: testCity,
-      },
-    };
+      const req = {
+        body: {
+          year: testYear,
+          city: testCity,
+        },
+      };
 
-    const res = {
-      send: mockSend,
-    };
+      const res = {
+        send: mockSend,
+      };
 
-    handler(req, res);
+      handler(req, res);
 
-    expect(mockSend).toHaveBeenCalledWith(
-      `You time-traveled to ${testCity}, in the year ${testYear}, where you did NOT find the princess!`
-    );
+      expect(mockSend).toHaveBeenCalledWith(
+        `You time-traveled to ${testCity}, in the year ${testYear}, where you found the princess!`
+      );
+    });
+
+    it("does not find the princess in Barcelona 1997", async () => {
+      const testYear = "1997";
+      const testCity = "Barcelona";
+
+      const req = {
+        body: {
+          year: testYear,
+          city: testCity,
+        },
+      };
+
+      const res = {
+        send: mockSend,
+      };
+
+      handler(req, res);
+
+      expect(mockSend).toHaveBeenCalledWith(
+        `You time-traveled to ${testCity}, in the year ${testYear}, where you did NOT find the princess!`
+      );
+    });
   });
 });
